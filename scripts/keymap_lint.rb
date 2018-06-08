94b24402ef9e6ef5e01b7bd677ee7f6a6eb4f986
Les Aker <me@lesaker.org>
2018-06-08 08:18:34 -0400

    add blanks

--- scripts/keymap_lint.rb
+++ scripts/keymap_lint.rb
@@ -3,13 +3,20 @@
 firmware = 'Model01-Firmware.ino'
 start_regex = /^KEYMAPS\($/
 end_regex = /^\) \/\/ KEYMAPS$/
-key_regec = / \w+,/
+key_regex = / (\w+,|/\*blank\*/)/
 
 orig_file = File.read(firmware)
 before, start_str, rest = orig_file.partition(start_regex)
 keymaps, end_str, after = rest.partition(end_regex)
 
 pad = keymaps.scan(key_regex).map(&:size).max - 2
+
+require 'pry'
+keymaps = keymaps.split("\n").each do |line|
+  next line unless line =~ /,/
+  binding.pry
+  raise
+end.join("\n")
 
 new_file = [
   before,
