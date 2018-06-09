b541b0cb0d6869511d5281e757fdfd828989dcb8
Les Aker <me@lesaker.org>
2018-06-09 09:34:02 -0400

    fix script for linting

--- scripts/keymap_lint.rb
+++ scripts/keymap_lint.rb
@@ -2,28 +2,26 @@
 
 firmware = 'Model01-Firmware.ino'
 start_regex = /^KEYMAPS\($/
-end_regex = /^\) \/\/ KEYMAPS$/
-key_regex = / (?:\w+,|\/\*blank\*\/)/
+end_regex = %r{^\) \/\/ KEYMAPS$}
+key_regex = %r{ (?:[(\w]+,|\/\*blank\*\/)}
 
 orig_file = File.read(firmware)
 before, start_str, rest = orig_file.partition(start_regex)
 keymaps, end_str, after = rest.partition(end_regex)
+pad = keymaps.scan(key_regex).map(&:size).max
 
-pad = keymaps.scan(key_regex).map(&:size).max - 2
-
-require 'pry'
 keymaps = keymaps.split("\n").map do |line|
-  next line unless line =~ /,/
-  '  ' + line.gsub(/,\s+/, ','.ljust(pad)).strip
+  next line unless line =~ key_regex
+  ' ' + line.scan(key_regex).map { |x| x.ljust(pad) }.join.rstrip
 end.join("\n")
 
 new_file = [
   before,
   start_str,
   keymaps,
+  "\n",
   end_str,
   after
 ].join
 
 File.open(firmware, 'w') { |fh| fh << new_file }
-
