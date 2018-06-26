e395338f1e4d56717931fe9dd2a97d6e9587abc7
Les Aker <me@lesaker.org>
2018-06-26 15:30:48 -0400

    update default flash path and readme

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ keyboard
 
 This repo holds my custom firmware for my [Keyboard.io](https://blog.keyboard.io). Details on how the firmware works and how to customize it can be found [in their upstream repo](https://github.com/keyboardio/Model01-Firmware).
 
+To build the firmware, run `make`. To flash, run `./scripts/flash.sh`.
+
--- scripts/flash.sh
+++ scripts/flash.sh
@@ -6,12 +6,12 @@ SCRIPT_DIR="$(python -c 'import os, sys; print(os.path.dirname(os.path.realpath(
 
 cd "$(dirname "$SCRIPT_DIR")"
 
-if [[ -z "${1:-}" ]] ; then
-    echo 'Must provide path to .hex file'
+export HEX_FILE_PATH="${1:-pkg/keyboard.hex}"
+
+if [[ ! -e "${HEX_FILE_PATH}" ]] ; then
+    echo "Firmware file not found: ${HEX_FILE_PATH}"
     exit 1
 fi
-
-export HEX_FILE_PATH="$1"
 
 ./board/libraries/Kaleidoscope/bin/kaleidoscope-builder flash
 
