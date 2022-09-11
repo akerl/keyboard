adf9b8083dd58ae2d269bca30b172ec724f2dd6d
Les Aker <me@lesaker.org>
2022-09-11 04:46:21 +0000

    test

--- keyboard.ino
+++ keyboard.ino
@@ -37,7 +37,7 @@ KEYMAPS(
   Key_RightShift,             Key_LeftAlt,                Key_Spacebar,               Key_RightControl,
   ShiftToLayer(FUNCTION)),
 
-  [FUNCTION] =  KEYMAP_STACKED
+  [FUNCTION] = KEYMAP_STACKED
   (___,                       Key_F1,                     Key_F2,                     Key_F3,                     Key_F4,                     Key_F5,                     ___,
   ___,                        ___,                        Key_Mute,                   Consumer_VolumeDecrement,   Consumer_VolumeIncrement,   Consumer_ScanNextTrack,     ___,
   Key_Home,                   ___,                        ___,                        ___,                        ___,                        Consumer_PlaySlashPause,
