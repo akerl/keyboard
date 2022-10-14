41a78d5f5130d0b123e914a951db2e24ef2ac90a
Les Aker <me@lesaker.org>
2022-10-14 19:32:34 +0000

    start splitting builds

--- scripts/keymap_lint.rb
+++ scripts/keymap_lint.rb
@@ -1,19 +1,22 @@
 #!/usr/bin/env ruby
 
-firmware = 'keyboard.ino'
+files = Dir.glob('*/*.ino')
+
 start_regex = /^KEYMAPS\($/
 end_regex = %r{^\) \/\/ KEYMAPS\($}
 key_regex = %r{ (?:[(\w]+,|\/\*blank\*\/)}
 
-orig_file = File.read(firmware)
-before, start_str, rest = orig_file.partition(start_regex)
-keymaps, end_str, after = rest.partition(end_regex)
-pad = keymaps.scan(key_regex).map(&:size).max
+files.each do |file|
+  orig_file = File.read(file)
+  before, start_str, rest = orig_file.partition(start_regex)
+  keymaps, end_str, after = rest.partition(end_regex)
+  pad = keymaps.scan(key_regex).map(&:size).max
 
-keymaps = keymaps.split("\n").map do |line|
-  next line unless line =~ key_regex
-  ' ' + line.scan(key_regex).map { |x| x.ljust(pad) }.join.rstrip
-end.join("\n")
+  keymaps = keymaps.split("\n").map do |line|
+    next line unless line =~ key_regex
+    ' ' + line.scan(key_regex).map { |x| x.ljust(pad) }.join.rstrip
+  end.join("\n")
 
-new_file = [before, start_str, keymaps, "\n", end_str, after].join
-File.open(firmware, 'w') { |fh| fh << new_file }
+  new_file = [before, start_str, keymaps, "\n", end_str, after].join
+  File.open(file, 'w') { |fh| fh << new_file }
+end
