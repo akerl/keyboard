eba470974ba78c6e9ee2cdd4b419547f5b51b60c
Les Aker <me@lesaker.org>
2022-10-22 12:34:17 -0400

    add flash

---
+++ scripts/flash.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+binfile="$1"
+
+if [[ ! -f "$binfile" ]] ; then
+    echo "File does not exist: $binfile"
+    exit 1
+fi
+
+device="$(arduino-cli board list | awk '/usbmodem/ { print $1 }')"
+
+if [[ -z "$device" ]] ; then
+    echo "Keyboard not found"
+    exit 1
+fi
+
+echo 'Start holding the PROG key (top left) and then hit enter. Keep holding PROG until the script completes'
+read
+
+arduino-cli upload -i "$binfile" -p "$device"
+
