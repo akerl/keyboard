77b4916bb66ba37bd0696395338b75825a6b72e9
Les Aker <me@lesaker.org>
2022-10-26 00:32:35 -0400

    fix whitespace

--- scripts/latest.sh
+++ scripts/latest.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 mkdir -p pkg
 
-url="$(curl -s 'https://api.github.com/repos/akerl/keyboard/releases/latest' | grep 'browser_download_url.*model100' | cut -d : -f 2,3 | tr -d \")"
+url="$(curl -s 'https://api.github.com/repos/akerl/keyboard/releases/latest' | grep 'browser_download_url.*model100' | cut -d : -f 2,3 | tr -d \"\ )"
 
 echo "Downloading ${url}"
 
