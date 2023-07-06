54084c959fab26ec6bf04b3a2708bd16ca5f15fa
Les Aker <me@lesaker.org>
2023-07-06 02:54:40 +0000

    use config file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-arduino-cli core update-index
-arduino-cli core install keyboardio:avr-tools-only arduino:avr keyboardio:gd32
+arduino-cli core update-index --config-file arduino-cli.yaml
+arduino-cli core install --config-file arduino-cli.yaml keyboardio:avr arduino:avr keyboardio:gd32
 
 ARDUINO_DATA="$(arduino-cli config dump | awk '/data/ { print $2 }')"
 ARDUINO_VERSION="$(arduino-cli board details -b keyboardio:avr:model01 | awk '/Board version/ { print $3 }')"
