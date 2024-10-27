22d20166970e129430c4d86bb226aac2d1bf313e
Les Aker <me@lesaker.org>
2024-10-27 11:02:50 -0400

    fix arduino commands

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 arduino-cli core update-index --config-file arduino-cli.yaml
 arduino-cli core install --config-file arduino-cli.yaml keyboardio:avr arduino:avr keyboardio:gd32
 
-ARDUINO_DATA="$(arduino-cli config dump | awk '/data/ { print $2 }')"
+ARDUINO_DATA="$(arduino-cli config get directories.data)"
 ARDUINO_VERSION="$(arduino-cli board details -b keyboardio:avr:model01 | awk '/Board version/ { print $3 }')"
 
 make -C "${ARDUINO_DATA}/packages/keyboardio/hardware/avr/${ARDUINO_VERSION}/libraries/Kaleidoscope" setup
